ba2bcf210f5a0b58a895c8613368526869fa6b10
Anna-Bala <annabala.contact@gmail.com>
2024-05-12 00:09:37 +0200

    Add HTML modal styling

--- apps/frontend/src/components/MapSections/index.ts
+++ apps/frontend/src/components/MapSections/index.ts
@@ -1,8 +1,7 @@
 import CodeBlocksSection from './CodeBlocksSection';
-import HTMLSection from './HTMLSection';
 import MenuSection from './MenuSection';
 import ResultSection from './ResultSection';
 import ScriptSection from './ScriptSection';
 import TaskSection from './TaskSection';
 
-export { CodeBlocksSection, HTMLSection, MenuSection, ResultSection, ScriptSection, TaskSection };
+export { CodeBlocksSection, MenuSection, ResultSection, ScriptSection, TaskSection };
--- apps/frontend/src/components/Modals/ModalWrapper.tsx
+++ apps/frontend/src/components/Modals/ModalWrapper.tsx
@@ -12,32 +12,36 @@ type TProps = {
   children: ReactNode;
   color: 'neutral' | 'green' | 'primary' | 'orange' | 'red';
   handleClose?: () => void;
+  isOpen: boolean;
   onPrimaryAction: () => void;
   primaryActionText: string;
   title: string;
 };
 
-const ModalWrapper = ({ children, color, handleClose, onPrimaryAction, primaryActionText, title }: TProps) => (
-  <div className={`modal-wrapper modal-wrapper-color__${color}`}>
-    <div className="modal-wrapper__header">
-      <Typography color={`${color}-700`} variant="subtitle2">
-        {title}
-      </Typography>
-      {handleClose && (
-        <IconButton icon={<CloseIcon size={20} fill={colors['color-neutral-900']} />} onClick={handleClose} />
-      )}
+const ModalWrapper = ({ children, color, handleClose, isOpen, onPrimaryAction, primaryActionText, title }: TProps) =>
+  isOpen ? (
+    <div className="modal-shadow">
+      <div className={`modal-wrapper modal-wrapper-color__${color}`}>
+        <div className="modal-wrapper__header">
+          <Typography color={`${color}-700`} variant="subtitle2">
+            {title}
+          </Typography>
+          {handleClose && (
+            <IconButton icon={<CloseIcon size={20} fill={colors['color-neutral-900']} />} onClick={handleClose} />
+          )}
+        </div>
+
+        <Divider />
+
+        <div className="modal-wrapper__main">{children}</div>
+
+        <div className="modal-wrapper__footer">
+          <Button color={color} variant="small" onClick={onPrimaryAction}>
+            {primaryActionText}
+          </Button>
+        </div>
+      </div>
     </div>
-
-    <Divider />
-
-    <div className="modal-wrapper__main">{children}</div>
-
-    <div className="modal-wrapper__footer">
-      <Button color={color} variant="small" onClick={onPrimaryAction}>
-        {primaryActionText}
-      </Button>
-    </div>
-  </div>
-);
+  ) : null;
 
 export default ModalWrapper;
