6e665f814a5f3ff93d21a2dd46f1052da365481a
Anna-Bala <annabala.contact@gmail.com>
2024-10-27 13:52:50 +0100

    Add level-save route

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -10,6 +10,8 @@ import { AuthModule } from './auth/auth.module';
 import { Level } from './typeorm/entities/Level';
 import { SeedModule } from './seed/seed.module';
 import { User } from './typeorm/entities/User';
+import { UserLevel } from './typeorm/entities/UserLevel';
+import { UserLevelModule } from './user-level/user-level.module';
 import { UsersModule } from './users/users.module';
 
 @Module({
@@ -21,6 +23,7 @@ import { UsersModule } from './users/users.module';
     AuthModule,
     UsersModule,
     SeedModule,
+    UserLevelModule,
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.MY_SQL_HOST,
@@ -28,7 +31,7 @@ import { UsersModule } from './users/users.module';
       username: process.env.MY_SQL_USERNAME,
       password: process.env.MY_SQL_PASSWORD,
       database: process.env.MY_SQL_DATABASE,
-      entities: [User, Level],
+      entities: [User, Level, UserLevel],
       synchronize: true,
     }),
   ],
---
+++ apps/backend/src/user-level/services/user-level.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+
+import { LevelSaveDto } from '../dtos/LevelSave.dto';
+import { UserLevel } from '../../typeorm/entities/UserLevel';
+
+@Injectable()
+export class UserLevelService {
+  constructor(
+    @InjectRepository(UserLevel)
+    private readonly userLevelRepository: Repository<UserLevel>,
+  ) {}
+
+  fetchLevelSave() {
+    return this.userLevelRepository.find();
+  }
+
+  async saveUserLevel(levelSaveDto: LevelSaveDto): Promise<UserLevel> {
+    const { userId, levelId, score } = levelSaveDto;
+
+    let levelSave = await this.userLevelRepository.findOne({
+      where: { user: { id: userId }, level: { id: levelId } },
+    });
+
+    if (levelSave) {
+      levelSave.score = score;
+    } else {
+      levelSave = this.userLevelRepository.create({
+        user: { id: userId },
+        level: { id: levelId },
+        score,
+      });
+    }
+
+    return this.userLevelRepository.save(levelSave);
+  }
+}
