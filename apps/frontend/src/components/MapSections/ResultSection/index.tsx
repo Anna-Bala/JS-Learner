99cc1fed39ef57ac858f42e943cd0ed924b4138b
Anna-Bala <annabala.contact@gmail.com>
2025-01-06 16:27:31 +0100

    Improve RWD for big desktops

--- apps/frontend/src/components/MapSections/ResultSection/index.tsx
+++ apps/frontend/src/components/MapSections/ResultSection/index.tsx
@@ -1,11 +1,25 @@
+import { isBigDesktop } from '../../utils';
 import './index.scss';
 
 type TProps = {
   resultIFrameSrcDoc: string;
 };
 
-const ResultSection = ({ resultIFrameSrcDoc }: TProps) => (
-  <iframe className="result-section" id="resultIframe" srcDoc={resultIFrameSrcDoc}></iframe>
-);
+const styles = `
+  <style>
+    body {
+      font-size: 18px;
+    }
+  </style>
+`;
+
+const ResultSection = ({ resultIFrameSrcDoc }: TProps) => {
+  const resultIFrameSrcDocWithStyling = `
+  ${isBigDesktop ? styles : ''}
+  ${resultIFrameSrcDoc}
+  `;
+
+  return <iframe className="result-section" id="resultIframe" srcDoc={resultIFrameSrcDocWithStyling}></iframe>;
+};
 
 export default ResultSection;
